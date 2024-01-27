eae16acf6b007e94305b2c98b485f600abf155be
William <williamandres0000@hotmail.com>
2024-01-27 14:49:35 -0500

    anim

--- Pandemonium/Assets/Scripts/PlayerController.cs
+++ Pandemonium/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f; // Velocidad de movimiento
-    
+    [SerializeField] private Animator animator;
     void Update()
     {
         // Obtener las entradas del teclado
@@ -15,5 +15,40 @@ public class PlayerController : MonoBehaviour
 
         // Mover el objeto
         transform.Translate(movement);
+       
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("Down", true);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Up", true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("Right", true);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Left", true);
+        }
+        //// 
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("Down", false);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("Up", false);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Right", false);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("Left", false);
+        }
     }
 }
